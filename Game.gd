7b301e366ca83be646eb2ee304fc093872f8ad58
MatheusPese <pese.ek.tk@outlook.com>
2019-03-11 12:25:51 -0300

    Changed Time StepTime call order

--- Game.gd
+++ Game.gd
@@ -9,10 +9,9 @@ onready var StepTimer = $StepTimer
 
 func _ready():
 	StepTimer.wait_time = 1.5 - (GameSpeed/10.0)
+	StepTimer.start()
 	
 	OS.set_window_size(Vector2(GRID_SIZE_IN_PIXELS.x * column, GRID_SIZE_IN_PIXELS.y * row))
-	
-	pass
 
 func _process(delta):
 	StepTimer.wait_time = 1.5 - (GameSpeed/10.0)
