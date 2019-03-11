c51d43f393dd906ac0a4bc09d677b68b9b7c26dd
MatheusPese <pese.ek.tk@outlook.com>
2019-03-11 15:43:23 -0300

    fixed function names, and added function to update screen block array.

--- Hand.gd
+++ Hand.gd
@@ -1,22 +1,43 @@
 extends Node2D
 
-onready var Game = get_tree().get_root().get_node("Game")
+onready var game = get_tree().get_root().get_node( "Game" )
+onready var row:int = self.position.y / game.GRID_SIZE_IN_PIXELS.y
+
+var side = 1
 
 func _input(event):
-	var LEFT = int(Input.is_action_just_pressed("ui_left"))
-	var RIGHT = int(Input.is_action_just_pressed("ui_right"))
-	
-	
 	if event is InputEventKey:
-		if Input.is_action_just_pressed("ui_up"):
-			self.rotation_degrees += 90
-		self.position += Vector2( (RIGHT-LEFT) * Game.GRID_SIZE_IN_PIXELS.x,0)
+		_rotate_shape_action()
+		_move_shape_horizontal_action()
+		_move_shape_vertical_action()	
 		
-		if Input.is_action_just_pressed("ui_down"):
-			self.position.y += Game.GRID_SIZE_IN_PIXELS.y
 
+func _rotate_shape_action():
+	if Input.is_action_just_pressed("ui_up"):
+		side = -side
+		self.rotation_degrees += 90 * side
 
+func _move_shape_horizontal_action():
+	update_screen_block_array()
+	
+	var LEFT:int = int( Input.is_action_just_pressed( "ui_left" ) )
+	var RIGHT:int  = int( Input.is_action_just_pressed( "ui_right" ) )
 
+	self.position += Vector2( ( RIGHT - LEFT ) * game.GRID_SIZE_IN_PIXELS.x, 0 )
+
+func _move_shape_vertical_action():
+	if Input.is_action_just_pressed( "ui_down" ):	
+		_shape_move_down()
 
 func _shape_move_down():
-	self.position.y += Game.GRID_SIZE_IN_PIXELS.y
+	update_screen_block_array()
+	row = self.position.y / game.GRID_SIZE_IN_PIXELS.y
+	
+	if  self.row + 1 != game.row:
+		self.position.y += game.GRID_SIZE_IN_PIXELS.y
+		
+
+func update_screen_block_array():
+	game.screen_block_array = game.create_2d_array(game.row, game.column, 0)
+	for i in get_child(0).shape_blocks_positions:
+		game.screen_block_array[i.x][i.y] = 1
